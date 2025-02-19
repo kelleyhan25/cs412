26be6c855217a761c09cacf5aeb8f7809c1bc664
Kelley <kelhan@bu.edu>
2025-02-18 23:42:43 -0500

    created show all page

--- mini_fb/urls.py
+++ mini_fb/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path 
 from django.conf import settings 
 from . import views 
+from .views import ShowAllProfilesView 
 
 urlpatterns = [
-    
+    path('', ShowAllProfilesView.as_view(), name='show_all_profiles'),
 ]
--- mini_fb/views.py
+++ mini_fb/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from .models import Profile
+from django.views.generic import ListView 
 # Create your views here.
+
+class ShowAllProfilesView(ListView):
+    '''subclass of listview to display all the user profiles'''
+    model = Profile 
+    template_name = 'mini_fb/show_all_profiles.html'
+    context_object_name = 'profiles'
