7671ce7dac89e1bcb6ea2fe5b744bbb943051722
Kelley <kelhan@bu.edu>
2025-02-23 19:40:27 -0500

    added example videos up to 6.6

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
+from django.urls import reverse
 # Create your views here.
 from .models import Article 
 import random
+from .forms import CreateArticleForm, CreateCommentForm
 
 class ShowAllView(ListView):
     '''Define a view class to show all blog Articles.'''
@@ -30,3 +32,23 @@ class RandomArticleView(DetailView):
         all_articles = Article.objects.all()
         article = random.choice(all_articles)
         return article
+    
+class CreateArticleView(CreateView):
+    '''A view to handle creation of a new Article.
+    (1) display the HTML form to user (GET)
+    (2) process the form submission and store the new Article object (POST)
+    '''
+
+    form_class = CreateArticleForm
+    template_name = "blog/create_article_form.html"
+
+class CreateCommentView(CreateView):
+    '''A view to create a new comment and save it to the database.'''
+
+    form_class = CreateCommentForm
+    template_name = "blog/create_comment_form.html"
+
+    def get_success_url(self) -> str:
+        '''Return the URL to redirect to after successfully submitting form.'''
+        return reverse('show_all')
+        ## note: this is not ideal, because we are redirected to the main page.
