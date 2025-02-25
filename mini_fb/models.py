2c4964ccd3613fcfd8a16c0e58ac1ad701f7031d
Kelley <kelhan@bu.edu>
2025-02-25 10:35:22 -0500

    task 1 a6

--- mini_fb/models.py
+++ mini_fb/models.py
@@ -19,3 +19,23 @@ class Profile(models.Model):
     def __str__(self):
         '''Returns string representation of this Profile user name'''
         return f'{self.first_name} {self.last_name}'
+    
+    def get_status_messages(self):
+        '''Return all of the status messages on this profile.'''
+
+        messages = StatusMessage.objects.filter(profile=self)
+        return messages
+
+class StatusMessage(models.Model):
+    '''Encapsulates the Facebook status message.'''
+
+    # data attributes of StatusMessage 
+    time_stamp = models.DateTimeField(auto_now=True)
+    message = models.TextField(blank=False)
+    profile = models.ForeignKey("Profile", on_delete=models.CASCADE)
+
+    def __str__(self):
+        '''Returns a string representation of the StatusMessage'''
+        return f'{self.message}'
+    
+
